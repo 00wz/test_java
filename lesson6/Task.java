88c4075017d08b26215bc4852ce5c28778d4cebd
Павел Тесленко <84148561+1wz@users.noreply.github.com>
2023-05-19 17:19:59 +0300

    w4

---
+++ lesson6/Task.java
@@ -0,0 +1,16 @@
+package lesson6;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Task {
+    HashMap<String,Pair<Inteder,Integer>> 
+    public static void main(String[] args) {
+        HashSet<Laptop> laptops=new HashSet<Laptop>();
+        laptops.add(new Laptop("Lenovo 770",17.3, 8, 256));
+        laptops.add(new Laptop("Apple air",14, 8, 1024));
+        laptops.add(new Laptop("Accer 111",16.0, 16, 2084));
+        laptops.add(new Laptop("HP povillion",15, 4, 256));
+    }
+    
+}
